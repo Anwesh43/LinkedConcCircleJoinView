8825ea52bc42f97f0ad19c3678a7da4eb6810ce4
Anwesh43 <anweshthecool0@gmail.com>
2019-03-31 12:31:52 +0530

    adding libraries and constants

--- conccirclejoinview/src/main/java/com/anwesh/uiprojects/conccirclejoinview/ConcCircleJoinView.kt
+++ conccirclejoinview/src/main/java/com/anwesh/uiprojects/conccirclejoinview/ConcCircleJoinView.kt
@@ -3,3 +3,22 @@ package com.anwesh.uiprojects.conccirclejoinview
 /**
  * Created by anweshmishra on 31/03/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.app.Activity
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.RectF
+
+val nodes : Int = 5
+val circles : Int = 2
+val parts : Int = 2
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#1565C0")
+val backColor : Int = Color.parseColor("#BDBDBD")

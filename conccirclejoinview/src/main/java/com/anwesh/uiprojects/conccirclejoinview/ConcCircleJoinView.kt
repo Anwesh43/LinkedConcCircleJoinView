778cc3e72e0f923050ae69e04e745a707d9d7e8d
Anwesh43 <anweshthecool0@gmail.com>
2019-03-31 12:54:40 +0530

    adding drawCCJNode canvas extension method

--- conccirclejoinview/src/main/java/com/anwesh/uiprojects/conccirclejoinview/ConcCircleJoinView.kt
+++ conccirclejoinview/src/main/java/com/anwesh/uiprojects/conccirclejoinview/ConcCircleJoinView.kt
@@ -22,6 +22,7 @@ val strokeFactor : Int = 90
 val sizeFactor : Float = 2.9f
 val foreColor : Int = Color.parseColor("#1565C0")
 val backColor : Int = Color.parseColor("#BDBDBD")
+val rUpFactor : Float = 0.1f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
@@ -29,3 +30,40 @@ fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.mirrorValue(a : Int, b : Int) : Float = (1 - scaleFactor()) * a.inverse() + scaleFactor() * b.inverse()
 fun Float.updateValue(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap
+
+fun Canvas.drawAtXY(x : Float, y : Float, cb : (Canvas) -> Unit) {
+    save()
+    translate(x, y)
+    cb(this)
+    restore()
+}
+
+fun Canvas.drawCCJNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = h / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    paint.color = foreColor
+    paint.style = Paint.Style.STROKE
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.strokeCap = Paint.Cap.ROUND
+    val sc1 : Float = scale.divideScale(0, 2)
+    val sc2 : Float = scale.divideScale(1, 2)
+    var r : Float = size
+    drawAtXY(w / 2, gap * (i + 1), {
+        drawCircle(0f, 0f, r, paint)
+        for (j in 0..(circles - 1)) {
+            val scj1 : Float = sc1.divideScale(j, circles)
+            val scj2 : Float = sc2.divideScale(j, circles)
+            val scj11 : Float = scj1.divideScale(0, 2)
+            val scj12 : Float = scj1.divideScale(1, 2)
+            val newR : Float = r * (1 - rUpFactor)
+            save()
+            rotate(90f * scj2)
+            drawLine(r, 0f, r - rUpFactor * r * scj11, 0f, paint)
+            drawArc(RectF(-newR, -newR, newR, newR), 0f, 360f * scj12, false, paint)
+            restore()
+        }
+    })
+
+}
